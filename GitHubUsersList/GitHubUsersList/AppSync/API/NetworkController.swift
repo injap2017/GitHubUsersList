7f813a8abd73b5eda8b97a0b8599e46d76a548d9
Edgar Sia <injap2017@yandex.com>
2021-01-08 12:07:04 +0800

    Cutome Decode

--- GitHubUsersList/GitHubUsersList/AppSync/API/NetworkController.swift
+++ GitHubUsersList/GitHubUsersList/AppSync/API/NetworkController.swift
@@ -19,32 +19,37 @@ class NetworkController {
     public func dataTask<T: Decodable>(with request: URLRequest, resultHandler: @escaping (Result<T, NetworkControllerError>) -> Void) {
         let urlTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard error == nil else {
-                resultHandler(.failure(.clientError))
+                DispatchQueue.main.async { resultHandler(.failure(.clientError)) }
                 return
             }
             
             guard let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode else {
-                resultHandler(.failure(.serverError))
+                DispatchQueue.main.async { resultHandler(.failure(.serverError)) }
                 return
             }
             
             guard let data = data else {
-                resultHandler(.failure(.noData))
+                DispatchQueue.main.async { resultHandler(.failure(.noData)) }
                 return
             }
             
             guard let decodedData: T = self.decodedData(data) else {
-                resultHandler(.failure(.dataDecodingError))
+                DispatchQueue.main.async { resultHandler(.failure(.dataDecodingError)) }
                 return
             }
             
-            resultHandler(.success(decodedData))
+            DispatchQueue.main.async { resultHandler(.success(decodedData)) }
         }
         
         urlTask.resume()
     }
     
     private func decodedData<T: Decodable>(_ data: Data) -> T? {
-        return try? JSONDecoder().decode(T.self, from: data)
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch (let error) {
+            print(error)
+            return nil
+        }
     }
 }
--- GitHubUsersList/GitHubUsersList/Models/UserDetails.swift
+++ GitHubUsersList/GitHubUsersList/Models/UserDetails.swift
@@ -7,16 +7,60 @@
 
 import Foundation
 
-struct UserDetails: Codable {
-    var id: Int = -1
-    var login: String = ""
-    var avatar_url: String = ""
-    var url: String = ""
-    var html_url: String = ""
-    var name: String = ""
-    var company: String = ""
-    var blog: String = ""
-    var followers: Int = 0
-    var following: Int = 0
-    var notes: String = ""
+class UserDetails: Codable {
+    var id: Int
+    var login: String
+    var avatar_url: String
+    var url: String
+    var html_url: String
+    var name: String
+    var company: String
+    var blog: String
+    var followers: Int
+    var following: Int
+    var notes: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case login = "login"
+        case avatar_url = "avatar_url"
+        case url = "url"
+        case html_url = "html_url"
+        case name = "name"
+        case company = "company"
+        case blog = "blog"
+        case followers = "followers"
+        case following = "following"
+    }
+    
+    required init(from decoder: Decoder) throws {
+        do {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            id = try container.decodeIfPresent(Int.self, forKey: .id) ?? -1
+            login = try container.decodeIfPresent(String.self, forKey: .login) ?? ""
+            avatar_url = try container.decodeIfPresent(String.self, forKey: .avatar_url) ?? ""
+            url = try container.decodeIfPresent(String.self, forKey: .url) ?? ""
+            html_url = try container.decodeIfPresent(String.self, forKey: .html_url) ?? ""
+            name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+            company = try container.decodeIfPresent(String.self, forKey: .company) ?? ""
+            blog = try container.decodeIfPresent(String.self, forKey: .blog) ?? ""
+            followers = try container.decodeIfPresent(Int.self, forKey: .followers) ?? 0
+            following = try container.decodeIfPresent(Int.self, forKey: .following) ?? 0
+            notes = ""
+        }
+    }
+    
+    init() {
+        id = -1
+        login = ""
+        avatar_url = ""
+        url = ""
+        html_url = ""
+        name = ""
+        company = ""
+        blog = ""
+        followers = 0
+        following = 0
+        notes = ""
+    }
 }
